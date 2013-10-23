f6aec1da798d850e833199061cf2fdf9872ae67e
mohammed sadiq <sadiqmmm@gmail.com>
2013-10-23 19:39:52 +0530

    Initail Fresh commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Anger::Application.config.secret_key_base = '10c0b9cef9edfb08adf5fd8f91465ad132a80709d8f91f4b92ee505e51b1d06074cbc4569e4faf22a264e970ec04845cdf406a8da3ddbfcd6f432117e2517788'
